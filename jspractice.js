77ae4921d8939c9aa00c1ed3cd28e0628e1dd960
Mahitha-139 <mahi135939@gmail.com>
2026-06-18 22:59:56 +0530

    Add files via upload

---
+++ jspractice.js
@@ -0,0 +1,13 @@
+let a=13;
+let b=19;
+op = "*";
+if (op == "+"){
+    console.log (a+b);
+}else if (op == "-"){
+    console.log (a-b);
+}else if (op == "*"){
+    console.log (a*b);
+}else if (op == "/"){
+    console.log (a/b);
+}
+console.log("Completed");
